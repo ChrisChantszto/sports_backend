935f5775bb258a4a3501a9084f1e673284cb2bc1
ChrisChantszto <chantszto.chris@gmail.com>
2023-11-11 00:44:35 +0800

    release

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from bson.objectid import ObjectId
 import os
 
 API = os.getenv('API')  # Fetch API from environment variables
-client = MongoClient(API)
 
 class SportDetails(BaseModel):
     format: Optional[str] = None
@@ -20,34 +19,12 @@ class SportData(BaseModel):
     zh: SportDetails
     en: SportDetails
 
-
 app = FastAPI()
 
 # initialize MongoDB client
 client = MongoClient(API)
 db = client['SportsApp']
 collection = db['sports']
-
-
-@app.get("/sports/{sport_type}")
-async def get_sport_data_from_db(sport_type):
-    sport_data = get_sport_data_from_db(sport_type)
-    if sport_data:
-        return sport_data
-    else:
-        raise HTTPException(status_code=404, detail="Sport type not found")
-    
-@app.get("/sports/{sport_type}/details", response_model=SportDetails)
-async def get_sport_details(sport_type: str):
-    sport_data = get_sport_data_from_db(sport_type)
-    if sport_data is not None and 'en' in sport_data:
-        return SportDetails(**sport_data['en'])
-    else:
-        raise HTTPException(status_code=404, detail="Sport not found")
-    
-@app.post("/sports/{sport_type}")
-async def update_sports_details(sport_type: str, sport_data: SportData):
-    return update_sport_data_in_db(sport_type, sport_data.model_dump())
 
 def get_sport_data_from_db(sport_type: str) -> Optional[Dict]:
     sport_data = collection.find_one({"sport_type": sport_type})
@@ -61,3 +38,22 @@ def update_sport_data_in_db(sport_type: str, sport_data: Dict) -> Dict:
     result = collection.insert_one(sport_data)
     return {"acknowledged": result.acknowledged, "_id": str(result.inserted_id)}
 
+@app.get("/sports/{sport_type}")
+async def get_sport_data(sport_type: str):
+    sport_data = get_sport_data_from_db(sport_type)
+    if sport_data:
+        return sport_data
+    else:
+        raise HTTPException(status_code=404, detail="Sport type not found")
+
+@app.get("/sports/{sport_type}/details", response_model=SportDetails)
+async def get_sport_details(sport_type: str):
+    sport_data = get_sport_data_from_db(sport_type)
+    if sport_data is not None and 'en' in sport_data:
+        return SportDetails(**sport_data['en'])
+    else:
+        raise HTTPException(status_code=404, detail="Sport not found")
+
+@app.post("/sports/{sport_type}")
+async def update_sports_details(sport_type: str, sport_data: SportData):
+    return update_sport_data_in_db(sport_type, sport_data.dict())
